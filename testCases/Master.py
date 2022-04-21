2136c2fe235857d3849233947ee4785b7ebbc6d9
VaghasiyaKrupal <vaghasiyakrupal@gmail.com>
2022-04-21 16:39:32 +0530

    Update

--- testCases/Master.py
+++ testCases/Master.py
@@ -10,7 +10,7 @@ from Locators.PracticeLocators import PracticeLocators
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-FilePath = "C:/Users/Administrator/PycharmProject/LegrandePython/TestData/Data.xlsx"
+FilePath = "TestData/Data.xlsx"
 datafile = load_workbook(FilePath)
 scriptData = datafile['Script Data']
 loginData = datafile['Login Credentials']
